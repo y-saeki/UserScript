c538a9b5940ab9159ab1b3c069890d94861fa992
Yoshihide Saeki <28527150+y-saeki@users.noreply.github.com>
2023-10-30 03:42:37 +0900

    match

--- ChatGPT/no-zoom-on-ios.user.js
+++ ChatGPT/no-zoom-on-ios.user.js
@@ -2,10 +2,10 @@
 // @name        No Zoom on iOS
 // @namespace   Violentmonkey Scripts
 // @icon        https://cdn.oaistatic.com/_next/static/media/apple-touch-icon.59f2e898.png
-// @match       https://chat.openai.com/c/*
+// @match       https://chat.openai.com/*
 // @grant       none
 // @noframes
-// @version     0.1
+// @version     0.1.1
 // @author      y-saeki
 // @supportURL  https://github.com/y-saeki/UserScript
 // @description disable input form auto-zoom function on iOS
